e481326b2146e95233c551fc87e1d80bcdd7ab6e
李光浩 <liguanghao@viatomtech.com>
2021-04-07 21:57:25 +0800

    dfu seccus

---
+++ app/src/main/java/com/aaa/df/DfuUpdateActivity.java
@@ -0,0 +1,15 @@
+package com.aaa.df;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DfuUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
--- app/src/main/java/com/aaa/df/MainActivity.kt
+++ app/src/main/java/com/aaa/df/MainActivity.kt
@@ -2,17 +2,42 @@ package com.aaa.df
 
 import android.Manifest
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.util.ArrayList
+import no.nordicsemi.android.dfu.DfuProgressListener
+import no.nordicsemi.android.dfu.DfuProgressListenerAdapter
+import no.nordicsemi.android.dfu.DfuServiceInitiator
+import no.nordicsemi.android.dfu.DfuServiceListenerHelper
+import java.io.File
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        DfuServiceInitiator.createDfuNotificationChannel(applicationContext);
         checkPermissions()
+
+        PathUtil.initVar(this)
+        File(PathUtil.getPathX("fuck")).writeBytes(byteArrayOf(0x32.toByte(), 0x98.toByte()))
+
+
+        val starter = DfuServiceInitiator("C3:10:54:BF:5B:F7")
+                .setDeviceName("DuoEK 2301")
+        val fileName: String = "f.zip"
+        val directory: String =PathUtil.filePath
+
+        starter.setZip(directory + fileName)
+        val controller = starter.start(applicationContext, DfuService::class.java)
+
     }
 
     private fun checkPermissions() {
@@ -34,4 +59,54 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    //DFU进度监听器
+    private val mDfuProgressListener: DfuProgressListener = object : DfuProgressListenerAdapter() {
+        override fun onDeviceConnecting(deviceAddress: String) {
+
+        }
+
+        override fun onDfuProcessStarting(deviceAddress: String) {
+
+        }
+
+        override fun onEnablingDfuMode(deviceAddress: String) {
+
+        }
+
+        override fun onFirmwareValidating(deviceAddress: String) {
+
+        }
+
+        override fun onDeviceDisconnecting(deviceAddress: String) {
+
+        }
+
+        override fun onDfuCompleted(deviceAddress: String) {
+            Toast.makeText(this@MainActivity, "固件升级成功", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onDfuAborted(deviceAddress: String) {
+
+        }
+
+        override fun onProgressChanged(deviceAddress: String, percent: Int, speed: Float, avgSpeed: Float, currentPart: Int, partsTotal: Int) {
+            val temp = percent * 360 / 100
+            Log.e("fuck", "sdf " + temp)
+        }
+
+        override fun onError(deviceAddress: String, error: Int, errorType: Int, message: String) {
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        DfuServiceListenerHelper.registerProgressListener(this, mDfuProgressListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        DfuServiceListenerHelper.unregisterProgressListener(this, mDfuProgressListener)
+    }
 }
