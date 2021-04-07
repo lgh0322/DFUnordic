3773d5812ec5c9d832bbbf3f9362f3a9083cdd0a
李光浩 <liguanghao@viatomtech.com>
2021-04-07 21:02:15 +0800

    Initial commit

---
+++ app/src/main/java/com/aaa/df/AA.kt
@@ -0,0 +1,11 @@
+package com.aaa.df
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.*
+
+class AA {
+
+}
---
+++ app/src/main/java/com/aaa/df/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.aaa.df
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.ArrayList
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        checkPermissions()
+    }
+
+    private fun checkPermissions() {
+        val permissions = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        val permissionDeniedList: MutableList<String> = ArrayList()
+        for (permission in permissions) {
+            val permissionCheck = ContextCompat.checkSelfPermission(this, permission)
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                permissionDeniedList.add(permission)
+            }
+        }
+        if (!permissionDeniedList.isEmpty()) {
+            val deniedPermissions = permissionDeniedList.toTypedArray()
+            ActivityCompat.requestPermissions(this, deniedPermissions, 12)
+        }
+    }
+
+}
